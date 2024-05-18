2f4fc02472f3161d3960617ef6cfdc45539fe105
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-05-18 15:28:22 +0300

    Add traces

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -2,6 +2,7 @@ import { BeforeAll, AfterAll, Before, After, Status } from '@cucumber/cucumber';
 import { type Browser, type BrowserContext, type Page } from '@playwright/test';
 import { join } from 'path';
 import { createLogger } from 'winston';
+import { readFileSync } from 'fs-extra';
 import { fixture } from './pageFixture';
 import contextManager from '../helper/context/contextManager';
 import { browserManager } from '../helper/browsers/browserManager';
@@ -10,7 +11,6 @@ import { options } from '../helper/util/logger';
 
 let browser: Browser;
 let context: BrowserContext;
-let page: Page;
 
 BeforeAll(async function () {
   getEnv();
@@ -19,32 +19,59 @@ BeforeAll(async function () {
 
 Before(async function ({ pickle }) {
   const scenarioName = `${pickle.name} ${pickle.id}`;
-  context = await browser.newContext();
-  page = await context.newPage();
+
+  context = await browser.newContext({
+    recordVideo: {
+      dir: join(process.cwd(), 'test-results', 'videos'),
+    },
+  });
+
+  await context.tracing.start({
+    name: scenarioName,
+    title: pickle.name,
+    sources: true,
+    screenshots: true,
+    snapshots: true,
+  });
+
+  const page = await context.newPage();
   fixture.page = page;
   fixture.logger = createLogger(options(scenarioName));
 });
 
 After(async function ({ pickle, result }) {
-  if (result.status !== Status.PASSED) {
-    const screenshotsPath = join(
-      process.cwd(),
-      'test-results',
-      'screenshots',
-      `${pickle.name.toLowerCase().replaceAll(/[-,]/g, '').split(' ').join('_')}.png`,
-    );
+  const screenshotsPath = join(
+    process.cwd(),
+    'test-results',
+    'screenshots',
+    `${pickle.name.toLowerCase().replaceAll(/[-,]/g, '').split(' ').join('_')}.png`,
+  );
 
-    const image = await fixture.page.screenshot({
-      path: screenshotsPath,
-      type: 'png',
-    });
+  // const videoPath = await fixture.page.video().path();
 
-    this.attach(image, 'image/png');
-  }
+  const tracePath = join(
+    process.cwd(),
+    'test-results',
+    'trace',
+    `${pickle.name.toLowerCase().replaceAll(/[-,]/g, '').split(' ').join('_')}.zip`,
+  );
+  const traceFileLink = `<a href="https://trace.playwright.dev/" target="_blank">Open ${tracePath}</a>`;
 
+  const image: Buffer = await fixture.page.screenshot({
+    path: screenshotsPath,
+    type: 'png',
+  });
+
+  await context.tracing.stop({ path: tracePath });
   await fixture.page.close();
   await context.close();
   contextManager.clear();
+
+  if (result.status === Status.PASSED) {
+    this.attach(image, 'image/png');
+    // this.attach(readFileSync(videoPath), 'video/webm');
+    this.attach(`Trace file: ${traceFileLink}`, 'text/html');
+  }
 });
 
 AfterAll(async function () {
